4d069b0d49eaa13e99d2ac79ceec1c74fc600a99
chris4a4 <chris4a4@gmail.com>
2025-01-15 15:32:10 -0800

    more debug

--- backend/api/forge/forge_tracker.py
+++ backend/api/forge/forge_tracker.py
@@ -18,20 +18,25 @@ class ForgeSlot:
             self.predicted_finish = start_time / 1000 + Config.RECIPES[item_id]['forge_time'] * (1 - QUICK_FORGE_REDUCTION)
             self.item_name = Config.ITEMS[item_id]["name"]
         else:
+            print(f"unknown item {item_id}")
             self.predicted_finish = None
             self.item_name = None
 
 
 # Check the API and update the current forging items and their webhooks
 def update_forge_tracker():
+    print("Updating forge tracker...")
     for user_dict in Config.TRACKING:
         profile_data = get_user_data(username=user_dict['username'], profile=user_dict['profile'])
 
         # Clear forge
         if 'forge' not in profile_data:
+            print("no forge")
             for slot_index in range(0, NUM_FORGE_SLOTS):
                 Config.tracker_data[user_dict['username']][slot_index + 1] = None
 
         # Update items
         for slot_num, slot_data in profile_data['forge']['forge_processes']['forge_1'].items():
             Config.tracker_data[user_dict['username']][slot_num] = ForgeSlot(slot_data['startTime'], slot_data['id'])
+    
+    print("Done updating forge tracker...")
